e5b985ae7e5442a0b1dd587b789a5c7a7c7404f6
Sarah <siti.sarah@ai.astra.co.id>
2021-09-14 22:28:07 +0700

    push

--- data/reqres/createUser.js
+++
@@ -1,5 +0,0 @@
-export const data = {
-  title: "recommendetion",
-  body: "motorcycle",
-  userId: 12,
-};
--- jest.setup.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable prefer-destructuring */
-require('jest-expect-message');
-const { matchers } = require('jest-json-schema');
-
-expect.extend(matchers);
-
-jest.setTimeout(100000);
